39db494325bb0173ee950bb8f3bb05e6bbfd99b7
JosuGamboa <Josugamboafer70@gmail.com>
2023-08-22 16:46:34 -0400

    version 2023-08-22.01

---
+++ juegos.py
@@ -0,0 +1,18 @@
+import Ahorcado
+import Adivinanza
+
+
+
+print('*********************************')
+print('Elija su Juego')
+print('*********************************')
+
+
+print('(1) Ahorcado (2) Adivinanza')
+
+juego = int(inpu("¿Cual juego?"))
+
+if (juego == 1):
+    print("Jugando Ahorcado")
+elif (juego == 2):
+    print("Jugando Adivinanza")
